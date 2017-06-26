b1886addf38e69f2a387878e8a02556e20719be4
AkyLau <phpcoldair@gmail.com>
2017-06-26 23:16:42 +0800

    增加banner图和登录注册组件

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="index">
         <div class="top-right links">
-            <v-btn class="primary white--text">Login</v-btn>
-            <v-btn class="red white--text">Register</v-btn>
+            <router-link class="primary white--text btn btn--dark btn--raised" :to="{ name: 'Login' }">Login</router-link>
+            <router-link class="red white--text btn btn--dark btn--raised" :to="{ name: 'Register' }">Register</router-link>
         </div>
+        <img src="../assets/images/001.png" width="400">
         <h2>Iwukua</h2>
         <ul>
-            <li><v-btn class="primary white--text" href="https://github.com/AkyLau/Pago">Pago</v-btn></li>
-            <li><v-btn class="primary white--text" href="https://github.com/AkyLau/iwukua">iwukua</v-btn></li>
-            <li><v-btn class="primary white--text" href="https://github.com/AkyLau/iwukua-vue">iwukua-vue</v-btn></li>
-            <li><a class="red white--text btn btn--dark btn--raised" v-ripple href="https://github.com/AkyLau/">My GitHub</a></li>
+            <li><a v-ripple class="primary white--text btn btn--dark btn--raised" href="https://github.com/AkyLau/Pago">Pago</a></li>
+            <li><a v-ripple class="primary white--text btn btn--dark btn--raised" href="https://github.com/AkyLau/iwukua">iwukua</a></li>
+            <li><a v-ripple class="primary white--text btn btn--dark btn--raised" href="https://github.com/AkyLau/iwukua-vue">iwukua-vue</a></li>
+            <li><a v-ripple class="red white--text btn btn--dark btn--raised" href="https://github.com/AkyLau/">My GitHub</a></li>
 
         </ul>
-
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
             path: '/',
             name: 'Index',
             component: Index
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            component: Register
         }
+
     ]
 })
