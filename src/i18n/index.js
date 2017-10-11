86aa79b204ce919d17f797b65f4bcaac6d6ad5c9
AkyLau <phpcoldair@gmail.com>
2017-10-11 22:45:07 +0800

    add some package

---
+++ src/i18n/index.js
@@ -0,0 +1,20 @@
+// load components and config
+import Vue from 'vue'
+import config from '@/config'
+import VueI18n from 'vue-i18n'
+
+const locales = ['zh-CN', 'en-US']
+Vue.use(VueI18n)
+let messages = {}
+
+locales.forEach(v => {
+    messages[v] = require(`./${v}/index`).default
+})
+
+const i18n = new VueI18n({
+    locale: helper.ls.get('locale', config.locale),
+    silentTranslationWarn: true,
+    messages
+})
+
+export default i18n
